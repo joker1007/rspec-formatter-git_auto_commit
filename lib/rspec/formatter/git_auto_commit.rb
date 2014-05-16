33b8fa0e749d14c9bdc9807be83c077f03c44970
joker1007 <kakyoin.hierophant@gmail.com>
2014-05-16 17:59:45 +0900

    Support RSpec3 formatter

--- lib/rspec/formatter/git_auto_commit.rb
+++ lib/rspec/formatter/git_auto_commit.rb
@@ -1,52 +1,12 @@
-require "rspec/core/formatters/base_text_formatter"
-
 module RSpec
   module Formatter
-    class GitAutoCommit < RSpec::Core::Formatters::BaseTextFormatter
-      GIT_PROG = ENV["GIT_BIN"] || "git"
-
-      def message(message)
-      end
-
-      def dump_failures
-      end
-
-      def dump_pending
-      end
-
-      def seed(number)
-      end
-
-      def dump_summary(duration, example_count, failure_count, pending_count)
-        state = failure_count == 0 ? "[green]" : "[red]"
-        summary = summary_line(example_count, failure_count, pending_count)
-        duration = "in #{format_duration(duration)}"
-        commit_message = [state, summary, duration].join(" ")
-
-        unless failed_examples.empty?
-          commit_message << "\n\nFailed Examples:\n\n"
-          failed_examples.each_with_index do |example, index|
-            commit_message << "#{short_padding}#{index.next}) #{example.full_description}\n\n"
-          end
-        end
-
-        system("#{GIT_PROG} add -u")
-        commit_message << "\n\n"
-        commit_message << `#{GIT_PROG} diff --cached`
-
-        commit_message << "\n\n"
-        commit_message << "File Status:\n"
-        commit_message << `#{GIT_PROG} status -s`
-
-        File.popen("#{GIT_PROG} commit -F -", "r+") do |fd|
-          fd.write commit_message
-          fd.close
-        end
-
-        log = `#{GIT_PROG} log --oneline -n 1`
-        output.puts "\nAuto committed.\n"
-        output.puts colorise_summary(log)
-      end
+    begin
+      require "rspec/core/formatters/legacy_formatter"
+      require "rspec/formatter/git_auto_commit_rspec3"
+      GitAutoCommit = GitAutoCommitRSpec3
+    rescue LoadError
+      require "rspec/formatter/git_auto_commit_rspec2"
+      GitAutoCommit = GitAutoCommitRSpec2
     end
   end
 end
---
+++ lib/rspec/formatter/git_auto_commit_rspec3.rb
@@ -0,0 +1,52 @@
+require "rspec/core/formatters/base_text_formatter"
+
+module RSpec
+  module Formatter
+    class GitAutoCommitRSpec3 < RSpec::Core::Formatters::BaseTextFormatter
+      RSpec::Core::Formatters.register self, :dump_summary
+
+      GIT_PROG = ENV["GIT_BIN"] || "git"
+
+      def seed(notification)
+      end
+
+      def dump_summary(summary)
+        state = summary.failure_count == 0 ? "[green]" : "[red]"
+        duration = "in #{format_duration(summary.duration)}"
+        commit_message = [state, summary.summary_line, duration].join(" ")
+
+        unless failed_examples.empty?
+          commit_message << "\n\nFailed Examples:\n\n"
+          failed_examples.each_with_index do |example, index|
+            commit_message << "#{short_padding}#{index.next}) #{example.full_description}\n\n"
+          end
+        end
+
+        system("#{GIT_PROG} add -u")
+        commit_message << "\n\n"
+        commit_message << `#{GIT_PROG} diff --cached`
+
+        commit_message << "\n\n"
+        commit_message << "File Status:\n"
+        commit_message << `#{GIT_PROG} status -s`
+
+        File.popen("#{GIT_PROG} commit -F -", "r+") do |fd|
+          fd.write commit_message
+          fd.close
+        end
+
+        log = `#{GIT_PROG} log --oneline -n 1`
+        output.puts "\nAuto committed.\n"
+
+        if summary.failure_count > 0
+          color_config = RSpec.configuration.failure_color
+        elsif summary.pending_count > 0
+          color_config = RSpec.configuration.pending_color
+        else
+          color_config = RSpec.configuration.success_color
+        end
+        output.puts color(log, color_config)
+      end
+    end
+  end
+end
